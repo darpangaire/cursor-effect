eb8695d12f1847c85e09dd737064a78a092cf613
darpan gaire <darpangaire11@gmail.com>
2024-10-17 12:40:37 +0545

    add local html css and js files

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
 let main = document.querySelector("#main");
-let cursor = document.querySelector("#cursor");
 let imageDiv = document.querySelector("#image");
+let cursor = document.querySelector("#cursor");
 
-main.addEventListener("mousemove", function (val) {
+main.addEventListener("mousemove", function (dets) {
   gsap.to(cursor, {
-    x: val.x,
-    y: val.y,
-    duration: 0.8,
+    x: dets.x,
+    y: dets.y,
+    duration: 0.6,
   });
 });
 
 imageDiv.addEventListener("mouseenter", function () {
-  cursor.innerHTML = "view more";
   gsap.to(cursor, {
-    scale: 1.5,
-    backgroundColor: "yellowgreen",
+    scale: 2,
     duration: 0.3,
+    backgroundColor: "yellowgreen",
   });
 });
 
-imageDiv.addEventListener("mouseleave", function () {
-  cursor.innerHTML = "";
+imageDiv.addEventListener("mouseout", function () {
   gsap.to(cursor, {
     scale: 1,
-    backgroundColor: "#fff",
     duration: 0.3,
+    backgroundColor: "#fff",
   });
 });
